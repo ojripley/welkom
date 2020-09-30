1cf2b9c1f99c0b106047554676ef58c196729fca
ojripley <ojripley19@gmail.com>
2020-09-30 10:30:57 -0400

    server boilerplate

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
 // express server goes here
+
+const PORT = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.urlencoded( {extended: true} ));
+const server = require('http').Server(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welkom to Welkom!');
+});
